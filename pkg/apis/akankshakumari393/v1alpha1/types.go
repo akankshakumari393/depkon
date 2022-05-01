169961e00cd06fdcd8d2518086eb360484d67ac4
Akanksha Kumari <akankshakumari393@gmail.com>
2022-05-01 10:36:05 +0530

    Add custom Resource to schemeBuilder

--- pkg/apis/akankshakumari393/v1alpha1/types.go
+++ pkg/apis/akankshakumari393/v1alpha1/types.go
@@ -4,6 +4,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type Depkon struct {
 	metav1.TypeMeta
 	metav1.ObjectMeta
@@ -18,4 +20,11 @@ type NamespaceConfig struct {
 	configmap  string
 	namespace  string
 	deployment []string
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type DepkonList struct {
+	metav1.TypeMeta
+	metav1.ObjectMeta
+	Items []Depkon
 }
